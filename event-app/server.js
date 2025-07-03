6c0159f41abd0c1e7dbb6dd44aa12dba2aa1d330
bLACKZU <satyakighosh65@gmail.com>
2025-07-03 23:53:37 +0530

    simple webapp

---
+++ event-app/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const dataStore = []; // 👈 holds submitted objects
+
+app.post('/data', (req, res) => {
+  console.log('Request received at /data')
+  console.log('Request body:', req.body)
+  dataStore.push(req.body)
+  console.log('Current dataStore:', dataStore)
+  res.json({ status: 'success', received: req.body })
+})
+
+app.get('/data', (req, res) => {
+  res.json(dataStore)
+})
+
+//routing path
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
